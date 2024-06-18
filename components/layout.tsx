c1e8aa57c8c245abc7839d6a708d6730fb69ceab
Yoland Y <4950057+yoland68@users.noreply.github.com>
2024-06-18 09:19:44 -0700

    Add meta tags for social media sharing

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,6 +9,8 @@ import mixpanel from 'mixpanel-browser'
 
 mixpanel.init(process.env.NEXT_PUBLIC_MIXPANEL_TOKEN as string, { track_pageview: true, persistence: 'localStorage' })
 
+import Image from 'next/image'
+
 export default function Layout({ children }: React.PropsWithChildren) {
     return (
         <>
@@ -16,7 +18,7 @@ export default function Layout({ children }: React.PropsWithChildren) {
                 <title>Comfy Org</title>
                 <meta
                     name="description"
-                    content="Building the ComfyUI ecosystem."
+                    content="Creators of ComfyUI. We are a team dedicated to iterate and improve ComfyUI, support the ComfyUI ecosystem with tools like node manager, node registry, cli, automated testing, and public documentation."
                 />
                 <link
                     rel="icon"
@@ -24,7 +26,18 @@ export default function Layout({ children }: React.PropsWithChildren) {
                     sizes="32x32"
                     href="https://storage.googleapis.com/comfy-assets/favicon.ico"
                 />
-                <ThemeModeScript />
+                <meta property="og:title" content="Comfy Org" />
+                <meta property="og:description" content="Creators of ComfyUI. We are a team dedicated to iterate and improve ComfyUI, support the ComfyUI ecosystem with tools like node manager, node registry, cli, automated testing, and public documentation." />
+                <meta property="og:image" content="/images/comfyui_screenshot.png" />
+                <meta property="og:image:alt" content="Comfy Org Banner" />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://comfy.org" />
+                <meta property="og:site_name" content="Comfy Org" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:title" content="Comfy Org" />
+                <meta name="twitter:description" content="Creators of ComfyUI. We are a team dedicated to iterate and improve ComfyUI, support the ComfyUI ecosystem with tools like node manager, node registry, cli, automated testing, and public documentation." />
+                <meta name="twitter:image" content="/images/comfyui_screenshot.png" />
+                <meta name="twitter:image:alt" content="Comfy Org Banner" />
             </Head>
             <Container>
                 <Header />
