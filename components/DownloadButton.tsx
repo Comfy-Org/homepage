d8a0cc8bb15a7ba4239e626576a37f10ebe2c23d
Yoland Y <4950057+yoland68@users.noreply.github.com>
2024-07-23 21:26:36 -0700

    Add all the download buttons

--- components/DownloadButton.tsx
+++ components/DownloadButton.tsx
@@ -2,35 +2,47 @@ import React, { useState, useEffect } from 'react';
 import { Button } from 'flowbite-react';
 
 const DownloadButton = () => {
-    const [downloadLink, setDownloadLink] = useState('');
-    const [osName, setOsName] = useState('');
+    // const [downloadLink, setDownloadLink] = useState('');
+    // const [osName, setOsName] = useState('');
 
-    useEffect(() => {
-        const detectOS = () => {
-            const userAgent = window.navigator.userAgent.toLowerCase();
-            if (userAgent.indexOf("win") > -1) {
-                setOsName('Windows');
-                setDownloadLink('https://docs.comfy.org/get_started/pre_package');
-            } else if (userAgent.indexOf("mac") > -1) {
-                setOsName('macOS');
-                setDownloadLink('https://docs.comfy.org/get_started/manual_install');
-            } else if (userAgent.indexOf("linux") > -1) {
-                setOsName('Linux');
-                setDownloadLink('https://docs.comfy.org/get_started/manual_install');
-            }
-        };
+    // TODO: detect OS to change the button color
+    // useEffect(() => {
+    //     const detectOS = () => {
+    //         const userAgent = window.navigator.userAgent.toLowerCase();
+    //         if (userAgent.indexOf("win") > -1) {
+    //             setOsName('Windows');
+    //             // setDownloadLink();
+    //         } else if (userAgent.indexOf("mac") > -1) {
+    //             setOsName('macOS');
+    //             // setDownloadLink('https://docs.comfy.org/get_started/manual_install');
+    //         } else if (userAgent.indexOf("linux") > -1) {
+    //             setOsName('Linux');
+    //             // setDownloadLink();
+    //         }
+    //     };
 
-        detectOS();
-    }, []);
+    //     detectOS();
+    // }, []);
 
     return (
-        <div className="inline-flex flex-col items-center">
-
+        <div className="inline-flex flex-row gap-2 items-center">
             <Button
-                href={downloadLink}
+                href='https://docs.comfy.org/get_started/pre_package'
                 className="mb-2"
             >
-                Download for {osName}
+                Download for Windows
+            </Button>
+            <Button
+                href='https://docs.comfy.org/get_started/manual_install'
+                className="mb-2"
+            >
+                Setup for MacOS
+            </Button>
+            <Button
+                href='https://docs.comfy.org/get_started/manual_install'
+                className="mb-2"
+            >
+                Setup for Linux
             </Button>
         </div>
     );
